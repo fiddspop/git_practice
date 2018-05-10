b853a5663ece459225a4d7b9401fb28e4baf6c1f
Ladd J McClurg <laddjmcclurg@icloud.com>
2018-05-10 11:11:55 -0600

    add git help menu_

---
+++ main.rb
@@ -0,0 +1,16 @@
+def put_git(cmd)
+    puts
+
+
+def menu
+    puts '1: Enter git command'
+    puts '2: Exit'
+    choice = gets.to_i
+
+case choice
+when 1 
+    puts 'Enter agit command'
+    puts_git(gets.strip)
+when 2 Exit
+    
+
