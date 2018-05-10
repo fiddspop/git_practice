0e90f0a8a51b18b6cfe3b08529740ed2cec2e17c
Ladd J McClurg <laddjmcclurg@icloud.com>
2018-05-10 12:03:50 -0600

    move git helper to own modue

--- main.rb
+++ main.rb
@@ -1,20 +1,26 @@
 require 'colorize'
-
-def put_git(cmd)
-    puts 'git #{cmd} -h'
+require_relative 'git'
 
 
-def menu
-    puts 'MAIN MENU'. COLORIZE(: CYAN)
-    puts '1: Enter git command'
-    puts '2: Exit'
-    choice = gets.to_i
 
-case choice
-when 1 
-    puts 'Enter a git command'
-    puts ' '
-    puts_git(gets.strip)
-when 2 Exit
+    def put_git(cmd)
+        puts 'git #{cmd} -h'
+
+
+    def self.menu
+        puts 'MAIN MENU'. COLORIZE(: cyan)
+        puts '1: Enter git command'
+        puts '2: Exit'
+        choice = gets.to_i
+
+    case choice
+    when 1 
+        puts 'Enter a git command'
+        puts ' '
+        puts_git(gets.strip)
+    when 2 Exit
+
+end
+
 
 
